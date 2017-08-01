195065a27d06483ef9cd87c957b6249894342050
dudulive <edurodriguesbraz@hotmail.com>
2017-07-31 22:41:21 -0300

    Introdução ao TypeScript para Angular

---
+++ hello-typescript/main.ts
@@ -0,0 +1,15 @@
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3];
+numeros.map(function(valor){
+    return valor * 3;
+});
+
+numeros.map(valor => valor * 3 );
+
+class Matematica {
+    soma(x,y){
+        return x + y;
+    }
+}
