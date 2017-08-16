804c476973872b31520a73735bf7a6448e56c7ea
dudulive <edurodriguesbraz@hotmail.com>
2017-08-15 22:38:07 -0300

    Diretivas: ngIf

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.css']
+  styleUrls: ['./diretiva-ngif.component.scss']
 })
 export class DiretivaNgifComponent implements OnInit {
+
+  cursos: string[] = [];
 
   constructor() { }
 
