00294c65f5b4192e2b05dde41b7acac4f173f1ab
dudulive <edurodriguesbraz@hotmail.com>
2017-08-31 21:53:44 -0300

    Comunicação entre componentes usando serviços (broadcast de eventos)

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -9,10 +9,10 @@ import { AppComponent } from './app.component';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
 
-
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,7 +1,11 @@
-import { Injectable} from '@angular/core';
+
+import { Injectable, EventEmitter} from '@angular/core';
 
 @Injectable()
 export class CursosService {
+
+    emitirCursoCriado = new EventEmitter<string>();
+    static criouNovoCurso = new EventEmitter<string>();
 
     private cursos: string[] = ['Angular 2', 'Java'];
 
@@ -11,5 +15,7 @@ export class CursosService {
 
     addCurso(curso: string){
         this.cursos.push(curso);
+        this.emitirCursoCriado.emit(curso);
+        CursosService.criouNovoCurso.emit(curso);
     }
 }
