48bc3f0f4e7fc7020e5f456be85c86270d2cd4fe
dudulive <edurodriguesbraz@hotmail.com>
2018-04-17 22:33:07 -0300

    Formulários reativos: Sincronizando HTML com FormGroup

--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -1,21 +1,21 @@
-import { CampoControlComponent } from './../campo-control/campo-control.component';
-import { FormDebugComponent } from './../form-debug/form-debug.component';
+
 import { FormsModule } from '@angular/forms';
 import { TemplateFormComponent } from './template-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SharedModule
   ],
   declarations: [
     TemplateFormComponent,
-    FormDebugComponent,
-    CampoControlComponent
+    
   ]
 })
 export class TemplateFormModule { }
