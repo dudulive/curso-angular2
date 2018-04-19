e94a198dc50b06fc42fdba7ccbe7feab192c097a
dudulive <edurodriguesbraz@hotmail.com>
2018-04-18 23:40:38 -0300

    Formulários reativos: Acesso ao FormControl no HTML e CSS de validação dos campos

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -75,4 +75,25 @@ export class DataFormComponent implements OnInit {
     this.formulario.reset();
   }
 
+  aplicaCssErro(campo: string) {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    };
+  }
+
+  verificaValidTouched(campo: string) {
+    return (
+      !this.formulario.get(campo).valid &&
+      (this.formulario.get(campo).touched || this.formulario.get(campo).dirty)
+    );
+  }
+
+  verificaEmailInvalido() {
+    const campoEmail = this.formulario.get('email');
+    if (campoEmail.errors) {
+      return campoEmail.errors['email'] && campoEmail.touched;
+    }
+  }
+
 }
